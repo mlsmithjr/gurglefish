9d98040d9141c3f18b4841735beb9731a2de7784
xobox123 <jarek1985305@op.pl>
2022-07-11 13:35:10 +0200

    Fix getting records to delete
    cur.execute returns None, it should be executed cur.fetchone() to get records to delete.

--- gurglefish/drivers/postgresql/Driver.py
+++ gurglefish/drivers/postgresql/Driver.py
@@ -269,7 +269,8 @@ class Driver(DbDriverMeta):
     def delete(self, cur, table_name: str, key: str):
         table_name = self.fq_table(table_name)
         try:
-            if cur.execute(f"SELECT id from {table_name} where id=%s", [key]) is None:
+            cur.execute(f"SELECT id from {table_name} where id=%s", [key])
+            if cur.fetchone() is None:
                 return 0
             cur.execute(f'delete from {table_name} where Id=%s', [key])
             return 1
